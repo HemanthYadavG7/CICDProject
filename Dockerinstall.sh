f23879a107114ec30093a858629ea11ce1480de0
HemanthYadavG7 <84517671+HemanthYadavG7@users.noreply.github.com>
2024-03-13 00:18:07 +0530

    Update Dockerinstall.sh

--- Dockerinstall.sh
+++ Dockerinstall.sh
@@ -2,5 +2,5 @@ sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo chkconfig docker on
-sudo usermod -aG docker ec2-user
+sudo usermod -aG docker ubuntu
 docker --version
